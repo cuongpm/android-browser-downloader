45ee81e5acb88179a5d788a34ab1eb9183fe14ce
mr.bo <videodownloader.player@gmail.com>
2018-05-02 07:44:47 -0700

    update all configs

--- app/src/main/java/vd/core/common/Constant.java
+++ app/src/main/java/vd/core/common/Constant.java
@@ -9,18 +9,18 @@ public class Constant {
     public final static String GOOGLE_PLAY_LINK = "https://play.google.com/store/apps/details?id=%s";
 
     // Remote data
-    public final static String REMOTE_URL = "";
+    public final static String REMOTE_URL = "https://videodownloader-8b54c.firebaseapp.com/api/";
 
     // Search url
     public final static String SUGGESTION_URL = "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=%s&format=5&alt=json";
 
     // Parser server
-    public final static String PARSER_SERVER = "http://video-downloader11.herokuapp.com/api/info?url=%s";
+    public final static String PARSER_SERVER = "http://bvd-dl.herokuapp.com/api/info?url=%s";
 
     // App config
-    public final static String UA_ID = "";
+    public final static String UA_ID = "UA-117583490-1";
 
-    public final static String AD_APP_ID = "";
-    public final static String AD_BANNER_ID = "";
-    public final static String AD_INTERSTITIAL_ID = "";
+    public final static String AD_APP_ID = "ca-app-pub-6693454642396900~1252074672";
+    public final static String AD_BANNER_ID = "ca-app-pub-6693454642396900/5927539057";
+    public final static String AD_INTERSTITIAL_ID = "ca-app-pub-6693454642396900/2726660641";
 }
